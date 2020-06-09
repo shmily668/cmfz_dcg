a77c38f14deca3fac8370276bde8467d62b577f7
“dcg <dingchengang5299@163.com>
2020-06-09 16:14:09 +0800

    v2.0

--- src/main/java/com/baizhi/CmfzDcgApplication.java
+++ src/main/java/com/baizhi/CmfzDcgApplication.java
@@ -10,6 +10,8 @@ public class CmfzDcgApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CmfzDcgApplication.class, args);
+
+        System.out.println("哈哈哈!");
     }
 
 }
